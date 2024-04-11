62e4d8aa1f5aa1be84c453d0f9f2f58b81152b76
Anna Harshyna <garsinaanna32778@gmail.com>
2024-04-11 13:56:09 +0300

    initial commit

---
+++ components/auth/auth-form.js
@@ -0,0 +1,89 @@
+import { useState, useRef } from 'react'
+import classes from './auth-form.module.css'
+
+async function createUser(email, password) {
+  try {
+    const response = await fetch('/api/auth/signup', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const data = await response.json() // an error occurs here
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!', data.message)
+    }
+
+    return data
+  } catch (error) {
+    console.error('Error creating user:', error)
+    throw error
+  }
+}
+
+function AuthForm() {
+  const [isLogin, setIsLogin] = useState(true)
+
+  const emailInputRef = useRef()
+  const passwordInputRef = useRef()
+
+  function switchAuthModeHandler() {
+    setIsLogin((prevState) => !prevState)
+  }
+
+  async function submitHandler(event) {
+    event.preventDefault()
+
+    const enteredEmail = emailInputRef.current.value
+    const enteredPassword = passwordInputRef.current.value
+
+    //TODO: add validation
+
+    if (isLogin) {
+      // log user in
+    } else {
+      try {
+        const result = await createUser(enteredEmail, enteredPassword)
+        console.log(result)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input
+            type='password'
+            id='password'
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default AuthForm
---
+++ pages/api/auth/signup.js
@@ -0,0 +1,44 @@
+import { hashPassword } from '../../../lib/auth'
+import { connectToDatabase } from '../../../lib/db'
+
+async function handler(req, res) {
+  if (req.method !== 'POST') return
+
+  console.log('HERE')
+
+  const data = req.body
+
+  const { email, password } = data
+
+  if (
+    !email ||
+    !email.includes('@') ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({ message: 'Invalid input' })
+  }
+
+  const client = await connectToDatabase()
+
+  const db = client.db() // getting access to a db
+
+  const existingUser = await db.collection('users').findOne({ email: email })
+
+  if (existingUser) {
+    res.status(422).json({ message: 'User already exists' })
+    client.close() // closing db connection
+    return
+  }
+
+  const hashedPassword = await hashPassword(password)
+
+  const result = await db
+    .collection('users')
+    .insertOne({ email: email, password: hashedPassword })
+
+  res.status(201).json({ message: 'User created' })
+  client.close()
+}
+
+export default handler
